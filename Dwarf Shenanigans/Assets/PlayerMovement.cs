fc5fe51ae3b5add7e61d61cc84339c77b012aff1
JLAgamer <jlagamer911@gmail.com>
2022-12-09 16:57:39 -0500

    feat: basic animations added

--- Dwarf Shenanigans/Assets/PlayerMovement.cs
+++ Dwarf Shenanigans/Assets/PlayerMovement.cs
@@ -7,10 +7,14 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
+    private SpriteRenderer sp;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
@@ -24,9 +28,14 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-
         moveInput.Normalize();
-
+        if (moveInput.x < 0) {
+            sp.flipX = true;
+        }
+        else if (moveInput.x > 0){
+            sp.flipX = false;
+        }
+        animator.SetFloat("MoveSpeed",Mathf.Max(Mathf.Abs(moveInput.x),Mathf.Abs(moveInput.y)));
         rb.velocity = moveInput * moveSpeed;
     }
 }
