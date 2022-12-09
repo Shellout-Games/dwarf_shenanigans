abed32529822ed78218680167c531f6aba8a619d
JLAgamer <jlagamer911@gmail.com>
2022-12-09 14:47:16 -0500

    feat: added lighting and player controls

--- Dwarf Shenanigans/Assets/PlayerMovement.cs
+++ Dwarf Shenanigans/Assets/PlayerMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        rb.velocity = moveInput * moveSpeed;
     }
 }
