cba7ca63b2f349577be5c991bda1a67b708b789a
JLAgamer <jlagamer911@gmail.com>
2023-01-28 16:46:54 -0500

    feat: integrated Unity Relay

---
+++ DwarfShenanigans/Assets/NetworkManagerUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private TestRelay testRelay;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button joinBtn;
+    [SerializeField] private TMP_InputField joinCodeFld;
+
+    private void Awake()
+    {
+        hostBtn.onClick.AddListener(() => { 
+            testRelay.CreateRelay(); 
+        });
+
+        joinBtn.onClick.AddListener(() => { 
+            testRelay.JoinRelay(joinCodeFld.text); 
+        });
+
+    }
+}
